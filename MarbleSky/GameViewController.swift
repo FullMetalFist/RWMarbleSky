5e1524e5abb9d6c4f101cfb069d1b6a04c5f515a
Michael Vilabrera <m.vilabrera@gmail.com>
2017-07-07 11:47:52 -0400

    possible corruption

--- MarbleSky/GameViewController.swift
+++ MarbleSky/GameViewController.swift
@@ -15,6 +15,14 @@ class GameViewController: UIViewController {
     
     var scnScene: SCNScene!
     
+    let CollisionCategoryBall = 1
+    let CollisionCategoryStone = 2
+    let CollisionCategoryPillar = 4
+    let CollisionCategoryCrate = 8
+    let CollisionCategoryPearl = 16
+    
+    var ballNode: SCNNode!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -31,10 +39,13 @@ class GameViewController: UIViewController {
         
         scnScene = SCNScene(named: "art.scnassets/game.scn")
         scnView.scene = scnScene
+        
+        scnScene.physicsWorld.contactDelegate = self
     }
     
     func setupNodes() {
-        
+        ballNode = scnScene.rootNode.childNode(withName: "ball", recursively: true)!
+        ballNode.physicsBody?.contactTestBitMask = CollisionCategoryPillar | CollisionCategoryCrate | CollisionCategoryPearl
     }
     
     func setupSounds() {
@@ -49,5 +60,27 @@ class GameViewController: UIViewController {
 extension GameViewController: SCNSceneRendererDelegate {
     func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
         
+    }
+}
+
+extension GameViewController: SCNPhysicsContactDelegate {
+    func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact) {
+        var contactNode: SCNNode!
+        if contact.nodeA.name == "ball" {
+            contactNode = contact.nodeB
+        } else {
+            contactNode = contact.nodeA
+        }
+        
+        if contactNode.physicsBody?.categoryBitMask == CollisionCategoryPearl {
+            contactNode.isHidden = true
+            contactNode.runAction(SCNAction.waitForDurationThenRunBlock(duration: 30, block: { (node: SCNNode!) -> Void in
+                node.isHidden = false
+            }))
+        }
+        
+        if contactNode.physicsBody?.categoryBitMask == CollisionCategoryPillar || contactNode.physicsBody?.categoryBitMask == CollisionCategoryCrate {
+            
+        }
     }
 }
